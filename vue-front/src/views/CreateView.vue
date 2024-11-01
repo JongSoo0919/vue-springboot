d65ac9f0fe5ce808b90c9dd8faa5e55564a9b9dd
KimJongSoo <prjs3643@gmail.com>
2024-11-02 00:52:54 +0900

    create 구현

--- vue-front/src/views/CreateView.vue
+++ vue-front/src/views/CreateView.vue
@@ -4,26 +4,26 @@
     <form>
       <div class="mb-3 mt-3">
         <label for="name" class="form-label">이름:</label>
-        <input type="text" class="form-control" id="name" placeholder="이름을 입력하세요." name="name">
+        <input type="text" class="form-control" id="name" placeholder="이름을 입력하세요." name="name" :class="{active: active.name }" v-model="result.name">
       </div>
       <div class="mb-3 mt-3">
         <label for="email" class="form-label">이메일:</label>
-        <input type="email" class="form-control" id="email" placeholder="이메일를 입력하세요." name="email">
+        <input type="email" class="form-control" id="email" placeholder="이메일를 입력하세요." name="email" :class="{active: active.email }" v-model="result.email">
       </div>
       <div class="mb-3">
         <label for="pwd" class="form-label">비밀번호:</label>
-        <input type="password" class="form-control" id="pwd" placeholder="비밀번호를 입력하세요." name="pwd">
+        <input type="password" class="form-control" id="pwd" placeholder="비밀번호를 입력하세요." name="pwd" :class="{active: active.pwd }" v-model="result.pwd">
       </div>
       <div class="d-flex">
         <div class="p-2 flex-fill">
           <div class="form-check">
-            <input type="radio" class="form-check-input" id="radio1" name="optradio" value="1" checked>남성
+            <input type="radio" class="form-check-input" id="radio1" name="optradio" value="1" checked v-model="result.gender">남성
             <label class="form-check-label" for="radio1"></label>
           </div>
         </div>
         <div class="p-2 flex-fill">
           <div class="form-check">
-            <input type="radio" class="form-check-input" id="radio2" name="optradio" value="2">여성
+            <input type="radio" class="form-check-input" id="radio2" name="optradio" value="2" v-model="result.gender">여성
             <label class="form-check-label" for="radio2"></label>
           </div>
         </div>
@@ -31,11 +31,67 @@
     </form>
     <div class="d-flex">
       <div class="p-2 flex-fill d-grid">
-        <a href="/user/findById" class="btn btn-primary">생성</a>
+<!--        <a href="/user/findById" class="btn btn-primary">생성</a>-->
+        <button class="btn btn-primary" @click="save">생성</button>
       </div>
       <div class="p-2 flex-fill d-grid">
-        <a href="/user" class="btn btn-primary">취소</a>
+<!--        <a href="/user" class="btn btn-primary">취소</a>-->
+        <router-link to="/user" class="btn btn-primary">취소</router-link>
       </div>
     </div>
   </div>
 </template>
+<script>
+import axios from "axios";
+
+export default {
+  name: 'CreateView',
+  data() {
+    return {
+      result: { name : '', email : '', pwd : '', gender : true },
+      active: {name: false, email: false, pwd: false}
+    }
+  },
+  created() {
+  },
+  methods: {
+    save() {
+      if(this.result.name === '') {
+        this.active.name = true;
+        return;
+      }else {
+        this.active.name = false;
+      }
+
+      if(this.result.email === '') {
+        this.active.email = true;
+        return;
+      }else {
+        this.active.email = false;
+      }
+
+      if(this.result.pwd === '') {
+        this.active.pwd = true;
+        return;
+      }else {
+        this.active.pwd = false;
+      }
+
+      axios.post('http://localhost:8080/save', this.result)
+          .then((res) => {
+            this.$store.commit('setUser', res.data.result);
+            this.$router.push({name: "ListView"})
+            // this.$router.push({name: "SelectView"})
+          })
+          .catch((err) => {
+            console.log(err)
+          })
+    }
+  }
+}
+</script>
+<style scoped>
+  .active {
+    background: blanchedalmond;
+  }
+</style>
